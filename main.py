5fcf3dbe47f694a13289d90968d6eff69091d923
LoganPaulus <82315449+LoganPaulus@users.noreply.github.com>
2022-04-03 01:42:01 +0300

    #1

---
+++ main.py
@@ -0,0 +1,21 @@
+def JahJaEiKüsimus(question):
+  JahJaEiVastus = input(question).upper()
+  if JahJaEiVastus == "JAH" or JahJaEiVastus == "EI":
+     return JahJaEiVastus
+  else:
+     return JahJaEiKüsimus(question)
+height  = float(input("Pange oma pikkus:")) 
+cm = (height) 
+vastus = JahJaEiKüsimus("Kas teil on pilet?")
+vastus1 = JahJaEiKüsimus("Kas teil on kaelakaart?")
+if height >=170:
+    print("Kahjuks te ei pääse kosmoselennule.")
+elif vastus1 == "JAH":
+  print("Palju õnne, pääsete kosmoselennule.") 
+elif vastus == "JAH":
+    print("Palju õnne, pääsete kosmoselennule.")
+else:
+    print("Kahjuks te ei pääse kosmoselennule.")  
+
+       
+
